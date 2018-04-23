68456b593287f827673451d7ef6bf4bff1741eeb
Martin Mendez <mendezmartin97@gmail.com>
2018-04-23 17:54:59 -0300

    Contexto armado

---
+++ conversorBinario.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	//abrir archivo
+	FILE* ftexto;
+	f = fopen("texto.txt","r");
+	FILE* fbin;
+	//crear archivo binario 
+	fb = fopen("binario.bin","wb");
+
+	while(!ftexto.eof())
+	{
+		
+	}
+
+
+	return 0;
+}
